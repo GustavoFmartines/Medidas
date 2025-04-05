c1bd5704fb243744b178d42e0895d9202405b102
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-04-05 20:44:10 -0300

    Foi criado um programa que faz a equivalência de metros para cm e km.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿        float Km;
+        int cm;
+        int metros;
+
+    Console.Clear();
+
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.BackgroundColor = ConsoleColor.Cyan;
+
+Console.WriteLine("--- Equivalência de Medidas --- ");
+
+    Console.ResetColor();
+
+
+Console.Write(@"
+Entre com a medida (em metros): ");
+    metros = Convert.ToInt32(Console.ReadLine()!);
+
+
+        Km = metros / 1000.0f;
+        cm = metros * 100;
+
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.BackgroundColor = ConsoleColor.Cyan;
+
+Console.WriteLine(@"
+--- Equivalencia ---");
+
+    Console.ResetColor();
+
+Console.WriteLine($"Metros: {metros}m");
+Console.WriteLine($"Centímetros: {cm}cm");
+Console.WriteLine($"Quilômetros: {Km}Km");
